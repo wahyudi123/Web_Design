217adae53b3898e17d4d9593935448ec55f8a700
Nur Wahyudi <nur.wahyudi93@yahoo.com>
2017-02-06 05:01:21 +0700

    commit pertama

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+     
+ 	$("#owl-banner-slider").owlCarousel({
+     
+ 	navigation : true, // Show next and prev buttons
+ 	slideSpeed : 300,
+ 	paginationSpeed : 400,
+ 	singleItem: true,
+ 	autoPlay: true
+ 	});
+     
+});
